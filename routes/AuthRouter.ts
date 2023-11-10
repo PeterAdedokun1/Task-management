1029fcef534efaf294babd83f8baa114aa2b8440
Adedokun peter <peteradedokun436@gmail.com>
2023-11-10 15:00:04 +0100

    change password

--- routes/AuthRouter.ts
+++ routes/AuthRouter.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 const router = Router();
-import { register , login, sendOtp, verifyOtp } from "../controllers/AuthController";
+import { register , login, sendOtp, verifyOtp, updatePassword } from "../controllers/AuthController";
 
 import { validateRegisterInput, validateLoginInput, validateSendOtp, } from "../middleware/ValidationMiddleWare";
 
@@ -8,4 +8,5 @@ router.post("/register", validateRegisterInput, register);
 router.post("/login",  validateLoginInput,login);
 router.post("/send-otp",validateSendOtp, sendOtp);
 router.post("/verify-otp", verifyOtp)
+router.post("/update-password", updatePassword)
 export default router
